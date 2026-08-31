3c83fde9d9472845c5edfc2b917dc0b2b2336a17
Kalle M. Aagaard <git@k-moeller.dk>
2026-08-31 07:30:42 +0200

    Version updated from 0.1.5.1 to 0.1.6

--- src/http_config/__init__.py
+++ src/http_config/__init__.py
@@ -5,7 +5,7 @@ from http_config.config import LimitConfig as LimitConfig
 from http_config.config import SSLConfig as SSLConfig
 from http_config.config import TimeoutConfig as TimeoutConfig
 
-__version__ = "0.1.5.1"
+__version__ = "0.1.6"
 
 
 __all__ = [
